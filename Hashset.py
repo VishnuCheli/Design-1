0da92e47f79df953ceb41a0329ee8da3bcc28e01
VishnuCheli <vishnucheliayan@gmail.com>
2022-09-25 01:24:56 -0400

    Change to TC and SC

--- Hashset.py
+++ Hashset.py
@@ -1,5 +1,5 @@
-#Time Complexity:: hashkey(): O(1), doublhashkey(): O(1), add(): O(n) at times, remove(): O(1), contains(): , Average: Moving towards O(1)
-#Space Complexity:: O(n) where n is the maximum number of elements
+#Time Complexity:: hashkey(): O(1), doublhashkey(): O(1), add(): O(1), remove(): O(1), contains(): O(1), Average: O(1)
+#Space Complexity:: O(n^2) where n*n is the maximum number of elements 10^6
 #Did this code successfully run on Leetcode : Yes
 #Any problem you faced while coding this : No
 
@@ -26,11 +26,11 @@ class MyHashSet(object):
         
         if self.primary[hash_index]==None: #checks if hashindex already has a secondary DS
             if hash_index == 0:
-                self.primary[hash_index] = [False]*(self.length + 1) #create a DS to store zero aswell
+                self.primary[hash_index] = [False]*(self.length + 1) #creating secondary DS with space to store zero aswell in the first index
             else:
-                self.primary[hash_index] = [False]*self.length #
+                self.primary[hash_index] = [False]*self.length #creating a secondary DS for more keys
         
-        self.primary[hash_index][double_hash_index] = True
+        self.primary[hash_index][double_hash_index] = True #adding element to hashset by changing False to True
     def remove(self, key):
         """
         :type key: int
@@ -39,18 +39,18 @@ class MyHashSet(object):
         hash_index = self.hashkey(key)
         double_hash_index = self.doublehashkey(key)
         
-        if self.primary[hash_index]!=None:
-            self.primary[hash_index][double_hash_index] = False
+        if self.primary[hash_index]!=None: #checks if there is a datastructure in the key's location
+            self.primary[hash_index][double_hash_index] = False #Changes key from True to False
         
     def contains(self, key):
         """
         :type key: int
         :rtype: bool
         """
-        hash_index = self.hashkey(key)
+        hash_index = self.hashkey(key) 
         double_hash_index = self.doublehashkey(key)
         
-        if self.primary[hash_index]!=None:
+        if self.primary[hash_index]!=None: #checks if there is datastructure at the key location, before checking for the key
             return self.primary[hash_index][double_hash_index]
 
 
